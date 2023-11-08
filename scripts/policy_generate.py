2d4ab9eb82a99e3486cf83b59e79ac07730d4655
Thomas Jackson <jacksontj.89@gmail.com>
2023-11-07 20:31:03 -0800

    Move object lookup to be generic
    This way you can reference anything within the namespace directory

--- scripts/policy_generate.py
+++ scripts/policy_generate.py
@@ -7,6 +7,22 @@ import os.path
 import json
 import yaml
 
+class ClusterNamespace(object):
+    def __init__(self, dirname):
+        self.dirname = dirname
+    def __getitem__(self, key):
+        return ClusterNamespaceKind(self.dirname, key)
+
+class ClusterNamespaceKind(object):
+    def __init__(self, dirname, kind):
+        self.dirname = dirname
+        self.kind = kind
+    def __getitem__(self, name):
+        fpath = os.path.join(self.dirname, self.kind+'-'+name+'.yaml')
+        with open(fpath) as fh:
+            o = yaml.safe_load(fh)
+            return o
+
 if __name__ == '__main__':
     with open('lib/imported/environments.json') as fh:
         environments = json.load(fh)
@@ -16,7 +32,7 @@ if __name__ == '__main__':
         with open(namespace_file) as fh:
             cluster_namespace = yaml.safe_load(fh)
             cluster_namespace['owners'] = cluster_namespace['metadata']['labels']['owners'].split(',')
-            env['namespace'] = cluster_namespace
+            env['obj'] = ClusterNamespace(os.path.join('releases', env['metadata']['labels']['cluster'], env['spec']['namespace']))
 
     environment = jinja2.Environment(loader=jinja2.FileSystemLoader("."))
     template = environment.get_template("scripts/policy.yml.tmpl")
@@ -26,6 +42,8 @@ if __name__ == '__main__':
     )
 
     # TODO: flag??
-    #print (content)
-    # reload and dump to clean up whitespace etc.
-    print (yaml.dump(yaml.safe_load(content)))
+    if False:
+        print (content)
+    else:
+        # reload and dump to clean up whitespace etc.
+        print (yaml.dump(yaml.safe_load(content)))
